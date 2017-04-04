3a026c48ffb25b5a03e21a12be0e6e8cb6420571
Gabriel Kaffka <Gabriel Kaffka>
2017-04-04 11:34:27 -0300

    Adds FrogJmp

---
+++ FrogJmp.java
@@ -0,0 +1,11 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int X, int Y, int D) {
+        return (int) Math.ceil((Y-X)/(double) D);
+    }
+}
