ab88e033f29274f35c85ebe31fff62b0d1d84676
Gabriel Kaffka <Gabriel Kaffka>
2017-04-04 11:24:43 -0300

    Replaces the algorithm for a clarity

--- PermMissingelement.java
+++ PermMissingelement.java
@@ -1,15 +1,18 @@
-// you can also use imports, for example:
- import java.util.*;
 
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
 
 class Solution {
     public int solution(int[] A) {
-       int result = A.length + 1;
-        for ( int i = 0; i < A.length; i++) {
-            result = result - A[i] + i + 1;
-        }
-        return result;
+        int b[] = new int[A.length+1];
+        int ideal = 0 ; 
+        int actual = 0 ;
+        
+        for (int i=0;i<A.length+1;i++) {
+            b[i] = i+1;
+            ideal += b[i];
+            if(i<A.length)
+		actual += A[i];   
+        }    
+ 
+        return ideal - actual;
     }
 }
